ad6f3c4489249d936aa71d55c04e21b234620524
unknown <morshedhasa1969@gmail.com>
2023-10-04 10:21:45 -0700

    era proble solved

--- Prob_18.cpp
+++ Prob_18.cpp
@@ -1,31 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int min_operations_to_satisfy_condition(int n, vector<int>& a) {
-    int operations = 0;
-    for (int i = n - 1; i >= 0; i--) {
-        operations = max(operations, a[i] - i);
-    }
-    return operations;
-}
 
 int main() {
     int t;
-    cin >> t; 
-    
+    cin >> t;
+
     while (t--) {
         int n;
         cin >> n;
-        
+
         vector<int> a(n);
         for (int i = 0; i < n; i++) {
             cin >> a[i];
         }
-        
-        int result = min_operations_to_satisfy_condition(n, a);
-        cout << result << endl;
+
+        int operations = 0;
+        int prev = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] > i + 1 + prev) {
+                operations += (a[i] - (i + 1 + prev));
+                prev += (a[i] - (i + 1 + prev));
+            }
+        }
+
+        cout << operations << endl;
     }
-    
+
     return 0;
 }
-
-//problemLink: https://codeforces.com/problemset/problem/1604/A
